848bde3b944b7164ed6471679479d3e50147fdbc
Raghav Malik <raghav1987@gmail.com>
2014-04-22 13:23:51 -0400

    first implementation of the Die class

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,13 +1,18 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
 # Input:
 # Output:
 # Steps:
+=begin
+initialize method -> 
+
+
+=end
 
 
 # 3. Initial Solution
@@ -15,14 +20,21 @@
 class Die
   def initialize(sides)
     # code goes here
+    if sides < 1
+    	raise ArgumentError.new("Number of sides should be greater than one homie")
+    else
+    	@sides = sides
+    end
   end
   
   def sides
     # code goes here
+    @sides
   end
   
   def roll
     # code goes here
+    1+rand(@sides)
   end
 end
 
@@ -30,16 +42,31 @@ end
 
 # 4. Refactored Solution
 
+=begin
 
-
-
-
+Very little code, don't think there needs to be any refactoring
+	
+=end	
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# Check if die.sides == sides
+sides = 5
+die = Die.new(sides)
+puts die.sides == sides
 
+# Check the possible range of the dice
+possible_range = (1..5).to_a
+unique_rolls = Array.new(5000) {die.roll}.uniq.sort
+puts unique_rolls == possible_range
 
+# Check if the initializer takes exactly one argument
+puts Die.instance_method(:initialize).arity == 1
 
+# 5. Reflection  
+=begin 
 
+Learnt how to write a simple class and instance methods. Learnt the rand method, and also how to
+raise exceptions. Also getting a better handle on reading rspec
 
-# 5. Reflection 
+=end
