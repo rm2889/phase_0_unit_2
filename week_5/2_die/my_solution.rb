76e10e4d0d827928c72c88b807fd9655255faff1
Raghav Malik <raghav1987@gmail.com>
2014-04-22 14:21:40 -0400

    finished week5question2

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -8,18 +8,51 @@
 # Input:
 # Output:
 # Steps:
+=begin
 
+1. initialize:
+Input: array of labels
+Output: no output
+Steps: 
+IF sides.is_a?(Array)
+	IF sides==[]
+		raise argumentException
+	ELSE
+		@sides = sides
+	END IF
+ELSE
+	raise argumentExeption -> need an array as an input
+END IF
+
+2. sides
+just returning the sides
+
+3. roll
+just return sides.sample :)
+=end
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+  	if labels.is_a?(Array)
+  		if labels == []
+  			raise ArgumentError.new("Need more than an empty array homie G")
+		else
+			@sides = labels.count
+			@labels = labels
+		end
+	else
+		raise ArgumentError.new("Need an array as an input playa")
+	end
   end
 
   def sides
+  	@sides
   end
 
   def roll
+  	@labels.sample
   end
 end
 
@@ -34,9 +67,27 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# Check if die.sides == number of labels
+labels = ('A'..'Z').to_a
+sides = labels.count
+die = Die.new(labels)
+puts die.sides == labels.count
 
+# Check the possible range of the dice
+possible_range = ('A'..'Z').to_a
+unique_rolls = Array.new(5000) {die.roll}.uniq.sort
+puts unique_rolls == possible_range
 
+# Check if the initializer takes exactly one argument
+puts Die.instance_method(:initialize).arity == 1
 
-
+#check if that argument is an array
+# puts Die.instance_method(:initialize).type == Array
+# HOW DO WE DO THIS? I.E. CHECK IF THE PARAMETER OF AN INSTANCE METHOD IS AN ARRAY?
 
 # 5. Reflection 
+
+=begin
+Nice to get some more practice with classes. New information learnt this time is the sample method for an array which returns a random element from the array
+Need to figure out how to get the parameter type (Array in this case) of an instance method
+=end
