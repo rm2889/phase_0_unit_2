bc28f7bc9ea15510fa96b9ea4a7724d24cc2b0a0
Raghav Malik <raghav1987@gmail.com>
2014-04-23 12:43:42 -0400

    finished making changes to bakery_num

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,7 +1,7 @@
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
 # Your Names
-# 1)
+# 1) Raghav Malik
 # 2)
 
  # This is the file you should end up editing. 
@@ -14,33 +14,34 @@ def bakery_num(num_of_people, fav_food)
   
   has_fave = false
 
-  my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
+  # instead of looping through each key, just do this:
+  fav = my_list[fav_food]
+  if fav
+    has_fave = true
   end
-  end
+  
   if has_fave == false
-  raise ArgumentError.new("You can't make that food")
+    raise ArgumentError.new("You can't make that food")
   else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
-    while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
-    else
-    cookie_qty = num_of_people
-    num_of_people = 0
-    end
-    end
-    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
+    # fav_food_qty = my_list.values_at(fav_food)[0]
+    fav_food_qty = my_list[fav_food] # easier to understand this than the line before
+    if num_of_people % fav_food_qty == 0
+      num_of_food = num_of_people / fav_food_qty
+      return "You need to make #{num_of_food} #{fav_food}(s)."
+    else num_of_people % fav_food_qty != 0
+      while num_of_people > 0
+        if num_of_people / my_list["pie"] > 0
+          pie_qty = num_of_people / my_list["pie"]
+          num_of_people = num_of_people % my_list["pie"]
+        elsif num_of_people / my_list["cake"] > 0
+          cake_qty = num_of_people / my_list["cake"]
+          num_of_people = num_of_people % my_list["cake"]
+        else
+          cookie_qty = num_of_people
+          num_of_people = 0
+        end
+      end
+      return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
     end
   end
 end
