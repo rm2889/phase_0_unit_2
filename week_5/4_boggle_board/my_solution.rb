71b69fab923b6843fa59cc852b304ee8f495cb95
Raghav Malik <raghav1987@gmail.com>
2014-04-23 10:14:35 -0400

    finished boggle board

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -13,34 +13,47 @@ boggle_board = [["b", "r", "a", "e"],
 # Pseudocode
 
 # Initial Solution
+  def create_word(board, *coords)
+    coords.map { |coord| board[coord.first][coord.last]}.join("")
+  end
 
-
+  
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3]) == "code" #=> returns "code"  
+puts create_word(boggle_board, [0,1], [0,2], [1,2]) == "rad"  #=> creates what california slang word?
 
 # Reflection 
-
+# Not really sure what's being asked in this part 1
 #-------------------------------------------------------------------------------
 
 # Part 2: Write a method that takes a row number and returns all the elements in the row.  
 
 # Pseudocode
+=begin
+boggle_board[row]
+
+}
+=end
 
 # Initial Solution
-
+def get_row(board,row)
+	return board[row]
+end
 
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
-
+puts get_row(boggle_board,1) == ["i", "o", "d", "t"]
+puts get_row(boggle_board,0) == ["b", "r", "a", "e"]
 
 # Reflection 
 
-
+# Quite straightforward, nothing to reflect really
 
 #-------------------------------------------------------------------------------
 
@@ -49,12 +62,18 @@ boggle_board = [["b", "r", "a", "e"],
 # Pseudocode
 
 # Initial Solution
+def get_col(board,col)
+	board.map { |row| row[col] }
+end
 
 
 # Refactored Solution
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+puts get_col(boggle_board,1)  ==  ["r", "o", "c", "a"]
+puts get_col(boggle_board,0) == ["b", "i", "e", "t"]
 
 
 # Reflection 
+# Spent some time trying to get my use of map to work -> i had board.map {|row| board[row][column]} and didn't realize that row is an array since every element is an array!
